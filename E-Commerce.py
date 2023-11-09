de5795e71d2544ad290b65eceeb43d967ad2c872
Uray Dika <103163726+UrayDika@users.noreply.github.com>
2023-11-09 08:48:07 +0700

    E-Commerce.py

--- E-Commerce.py
+++ E-Commerce.py
@@ -13,7 +13,7 @@ def create_bycity_df(df):
     
     return bycity_df
 
-all_df = pd.read_csv("https://media.githubusercontent.com/media/UrayDika/Data-Analyst/main/E-Commerce_Revenue.csv?token=AYTCOTVJH2HUQ63ET22C6PTFJRDJY.csv")
+all_df = pd.read_csv("https://media.githubusercontent.com/media/UrayDika/Data-Analyst/main/E-Commerce_Revenue.csv?token=AYTCOTSQ7WDEYDLOGIYOQV3FJRD7W")
 
 def create_rfm_df(df):
     rfm_df = df.groupby(by="customer_city", as_index=False).agg({
